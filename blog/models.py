0ed02f22cacaa5d343ac1e64ce447b3606cb2908
EdwardDaChina <edwardda.china@outlook.com>
2019-05-04 22:22:40 +0800

    使url规范化

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -25,6 +26,10 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()  # 默认的管理器
     published = PublishedManager()  # 自定义管理器
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
 
 class Meta:
     ordering = ('-publish',)
