e08c62a62fd4b75385185b12cbe4981f70b0a183
EdwardDaChina <edwardda.china@outlook.com>
2019-05-05 17:17:10 +0800

    发送邮件测试失败

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,10 +123,10 @@ STATIC_URL = '/static/'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_PORT = 25
+
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True   #是否使用TLS安全传输协议
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_HOST_USER = '1002366833@qq.com'
-EMAIL_HOST_PASSWORD = 'dqbysh11'
-DEFAULT_FROM_EMAIL = '1002366833@qq.com'
+EMAIL_HOST_PASSWORD = 'fizeajjcnowrbddj'
+
