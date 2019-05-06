cc754af2890f9760ed32fb86e1aaaf06c071e52a
EdwardDaChina <edwardda.china@outlook.com>
2019-05-06 10:03:46 +0800

    upload

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@
 
 # Create your models here.
 
-
+from taggit.managers import TaggableManager
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -26,6 +26,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()  # 默认的管理器
     published = PublishedManager()  # 自定义管理器
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,12 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 
 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
 def post_share(request, post_id):
     # 通过id 获取 post 对象
     post = get_object_or_404(Post, id=post_id, status='published')
@@ -70,8 +76,5 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'page': page,'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month,
-                             publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
 
